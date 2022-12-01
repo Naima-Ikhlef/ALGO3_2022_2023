c543de61f9922d0c9ad3d76eb34ce2f9c97764a8
Naima-Ikhlef <113034327+Naima-Ikhlef@users.noreply.github.com>
2022-12-01 09:08:01 +0100

    Create main.c

---
+++ TP 4/main.c
@@ -0,0 +1,13 @@
+int main(int argc, char const *argv[])
+{
+    FILE *f=NULL;
+    f=fopen("test.txt","r+");
+    Client c;
+    int num=0;
+    char _nom[15];
+    afficherClientFichier(f,c);
+    num = nombreClientsFichier(f);
+    printf("il y a : %d", num);
+    appartientClientFichier(f, _nom);
+    return 0;
+}
