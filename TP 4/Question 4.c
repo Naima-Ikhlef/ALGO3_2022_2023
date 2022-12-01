1d98226d905d19c0b2dd8157c0bd72cad112f6f2
Naima-Ikhlef <113034327+Naima-Ikhlef@users.noreply.github.com>
2022-12-01 09:05:03 +0100

    Create Question 4.c

---
+++ TP 4/Question 4.c
@@ -0,0 +1,14 @@
+int nombreClientsFichier(FILE *f)
+{
+    f=fopen("test.txt","r+");
+    fseek(f, 0, SEEK_SET);
+    int id =0;
+    char nom[12];
+    int compte=0;
+    while (!feof(f))
+    {
+        fscanf(f,"%d %s", &id, &nom);
+        compte +=1;
+    }
+    return compte;
+}
